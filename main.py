7005b9b3b73152a77947773be285cad1bf8d1d63
chalexyur <chalexyur@gmail.com>
2018-04-07 01:02:23 +0300

    select lessons

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QTableWidget, QTableWidgetItem
 from PyQt5 import uic
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import *
 from mysql.connector import MySQLConnection, Error
 from bs4 import BeautifulSoup
 import urllib.request
@@ -41,8 +43,15 @@ class MyApp(QMainWindow):
         self.ui.dwnldButton.clicked.connect(self.download)
         self.ui.parseButton.clicked.connect(self.parse)
         self.ui.updGlButton.clicked.connect(self.updateGrouplist)
+        self.ui.toTablesButton.clicked.connect(self.toTables)
+        self.statusBar().addWidget(self.ui.progressBar)
+        self.ui.progressBar.hide()
 
     def updateGrouplist(self):
+        self.ui.groupComboBox.clear()
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
         cursor.execute("SELECT name,code,year FROM groups")
         grouplist = cursor.fetchall()
         print(type(grouplist))
@@ -51,6 +60,7 @@ class MyApp(QMainWindow):
             print(type(group))
             print('-'.join(map(str, group)))
             self.ui.groupComboBox.addItem('-'.join(map(str, group)))
+        conn.close()
 
     def download(self):
         print("downloading...")
@@ -63,28 +73,65 @@ class MyApp(QMainWindow):
                 print(link)
         urllib.request.urlretrieve(link, "files/1.xlsx")
 
+    def toTables(self):
+        self.ui.groupComboBox.clear()
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
+        cursor.execute("SELECT type, title, teacher, room FROM lessons WHERE day=1 AND even=1 AND `group`='ИКБО-06-16'")
+        print("exec")
+        lessons = cursor.fetchall()
+        conn.close()
+
+        for i in range(6):
+            for j in range(4):
+                self.ui.tableWidget1.setItem(i, j, QTableWidgetItem(lessons[i][j]))
+        self.ui.tableWidget1.setColumnWidth(0, 30)
+        self.ui.tableWidget1.setColumnWidth(1, 170)
+        self.ui.tableWidget1.setColumnWidth(2, 130)
+        self.ui.tableWidget1.setColumnWidth(3, 50)
+
     def parse(self):
         print("parsing...")
+        self.ui.progressBar.show()
+        self.ui.progressBar.setValue(0)
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
         from openpyxl import load_workbook
         wb = load_workbook(filename='files/1.xlsx', read_only=True)
         ws = wb['Лист1']
 
+        x = 0
+        y = 0
+        for row in ws.iter_rows(min_row=2, max_row=2, min_col=1, max_col=200):
+            for cols in row:
+                print(cols.value)
+                if (cols.value == self.ui.groupComboBox.currentText()):
+                    y = cols.row
+                    x = cols.column
+                    print(x, y, "!!!!!!!!!!!!!!!!111111111111111111111111111111111")
+                    break
+
         mir = 4
         mar = mir + 71
-        mic = 6
+        mic = x
         mac = mic + 3
-        gr = ws['F2'].value
+        gr = ws.cell(row=y, column=x).value
+        print(gr)
         i = 0
         number = 1
 
+        pr = 1
+
         for row in ws.iter_rows(min_row=mir, max_row=mar, min_col=mic, max_col=mac):
-            # print(row[0].value,row[1].value,row[2].value,row[3].value)
-            print(i)
+            pr += 2
+            self.ui.progressBar.setValue(pr)
             day = i // 12 + 1
             if (number > 6):
                 number = 1
             if (i % 2 == 0):
-                even = 0;
+                even = 0
             else:
                 even = 1
             try:
@@ -95,15 +142,13 @@ class MyApp(QMainWindow):
                 print(error)
             i += 1
             number += even
-
+        self.ui.progressBar.setValue(100)
+        conn.close()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
     window.show()
-    dbconfig = read_db_config()
-    conn = MySQLConnection(**dbconfig)
-    cursor = conn.cursor()
 
     sys.exit(app.exec_())
